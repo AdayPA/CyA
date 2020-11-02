27b64ab335843326a84c8dbda45c7167d6a21b11
AdayPA <dyto_aday@hotmail.com>
2020-11-02 13:55:17 +0000

    test

--- Practica_5/Set.cc
+++ Practica_5/Set.cc
@@ -55,7 +55,7 @@ void Set::Work(std::string in) {
 std::vector<unsigned long int> Set::Range(std::vector<int> in){
   unsigned long int result = 0;
   int vect_pos = floor(in[2] / 64);
-  
+  //test
   
 }
 
